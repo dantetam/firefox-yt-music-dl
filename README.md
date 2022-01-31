d9407d9e22002f73fdc1d3fe1cfddbe415b22980
Dante Tam <dante.tam1@gmail.com>
2022-01-30 21:30:57 -0600

    My music is ready

---
+++ README.md
@@ -0,0 +1,15 @@
+Firefox Favorites JSON Music Downloader
+============
+This is a code doodle that automatically downloads music from YouTube links in my Firefox bookmarks. I have a habit of putting songs I like into the toolbar and forgetting about them.
+
+Put your Firefox bookmarks .json file path into the variable `chosenBookmarkFileName`. Run the program. The program will never download files that are already present in the folders `output` and `ignore`.
+
+
+Tech Details
+-----------------
+Bookmark data is represented in a nested JSON format with an arbitrary number of nested folders containing bookmarks and folders. Bookmarks contain the title of the website, the URL, and the time of their creation. The [YouTube DL library](https://github.com/ytdl-org/youtube-dl) and [its more convenient fork](https://github.com/yt-dlp/yt-dlp/) are APIs used to generate downloadable media links which this program uses to download music, and also provide data about the YouTube video such as its duration. This program specifically downloads audio only.
+
+
+Quandaries
+-----------------
+Google has grown to be one of the most profitable companies in history by scraping the web and loading other people's servers without permission and any promise of a royalty beyond exposure. If anything, this saves them bandwidth, especially if I turned on my ad blocker.
---
+++ main.py
@@ -0,0 +1,91 @@
+import json
+import subprocess
+import os
+import urllib.request
+import re
+from pathlib import Path
+
+
+chosenBookmarkFileName = "./bookmarks/bookmarks-2022-01-29.json"
+downloadLimit = 100
+
+
+def createFolderIfNonexistent(path):
+    existingFile = Path(path)
+    if not existingFile.is_file():
+        os.makedirs(path)
+
+def traverseBookmarks(item):
+    return traverseBookmarksHelper([], item)
+
+def traverseBookmarksHelper(result, item):
+    if not item: return
+    if "children" in item:
+        for i in range(len(item["children"])):
+            traverseBookmarksHelper(result, item["children"][i])
+    elif "uri" in item:
+        uri = item["uri"]
+        title = item["title"]
+        #Remove special characters that mess with Windows's filesystem
+        title = title.translate(str.maketrans("", "", '?<>|\"*:\\/'))
+        print(title.encode("utf-8"), flush=True)
+        time = item["dateAdded"]
+        if "youtube.com" in uri:
+            ytBookmark = dict()
+            ytBookmark["uri"] = uri
+            ytBookmark["title"] = title
+            ytBookmark["time"] = time
+            result.append(ytBookmark)
+    return result
+
+def downloadMusicOrUseCache(url, title):
+    try:
+        outputPath = "./output/" + title + ".mp3"
+        existingFile = Path(outputPath)
+        existingIgnoredFile = Path("./ignore/" + title + ".mp3")
+        if not existingFile.is_file() and not existingIgnoredFile.is_file():
+            ytAudioData = os.popen("yt-dlp.exe -x --get-url \"" + url + "\" --get-duration").readlines()
+            ytAudioUrl = ytAudioData[0]
+
+            #Do not download files that are obviously way too long or short
+            ytDurationString = ytAudioData[1]
+            tokens = ytDurationString.split(":")
+            minutes = int(tokens[0])
+
+            if minutes >= 1 and minutes < 8 and len(tokens) <= 2: #"2 hours 9 minutes 20 seconds => [2,9,20]"
+                print("Downloading song title: ", flush=True)
+                print(str(title).encode("utf-8"), flush=True)
+                print("", flush=True)
+                urllib.request.urlretrieve(ytAudioUrl, outputPath)
+                return True
+            else:
+                print("File too long or too short", flush=True)
+        else:
+            print("Found file in cache", flush=True)
+    except Exception as e:
+        print(e, flush=True)
+    print("Skipping: ", flush=True)
+    print(str(title).encode("utf-8"), flush=True)
+    print("", flush=True)
+    return False
+
+
+createFolderIfNonexistent("./output/")
+createFolderIfNonexistent("./ignore/")
+
+data = {}
+with open(chosenBookmarkFileName, encoding="utf8") as bookmarkFile:
+    data = json.load(bookmarkFile)
+    #print(str(data).encode("utf-8"))
+    ytBookmarks = traverseBookmarks(data)
+    #print(str(ytBookmarks).encode("utf-8"))
+
+    for ytBookmark in ytBookmarks:
+        #print(str(ytBookmark).encode("utf-8"))
+        actuallyDownloadedFile = downloadMusicOrUseCache(ytBookmark["uri"], ytBookmark["title"])
+        if actuallyDownloadedFile:
+            downloadLimit = downloadLimit - 1
+            if downloadLimit == 0:
+                break
+
+print("Downloads complete", flush=True)
